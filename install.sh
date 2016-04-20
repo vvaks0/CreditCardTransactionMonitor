bc825194322a0ba55f8e8447aaa1bbfcf6c0cd84
vvaks <vvaks@hortonworks.com>
2016-04-20 04:31:46 +0100

    added automation

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
+sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
+yum install -y apache-maven
+
 cd CreditCardTransactionMonitor
 mvn clean package
 cp -vf target/CreditCardTransactionMonitor-0.0.1-SNAPSHOT.jar /home/storm
 
 #Start Kafka
-#!/bin/bash
 
 KAFKASTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/KAFKA | grep '"state" :' | grep -Po '([A-Z]+)')
 
@@ -41,6 +44,36 @@ fi
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic IncomingTransactions
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper sandbox.hortonworks.com:2181 --replication-factor 1 --partitions 1 --topic CustomerTransactionValidation
 
+#Start HBASE
+HBASESTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/HBASE | grep '"state" :' | grep -Po '([A-Z]+)')
+
+if [ "$HBASESTATUS" == INSTALLED ]; then
+	echo "Starting  Hbase Service..."
+	TASKID=$(curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Hbase via REST"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/HBASE | grep "id" | grep -Po '([0-9]+)')
+	echo "HBASE TaskId " $TASKID
+	sleep 2
+
+	LOOPESCAPE="false"
+
+	until [ "$LOOPESCAPE" == true ]; do
+
+		TASKSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+		if [ "$TASKSTATUS" == COMPLETED ]; then
+			LOOPESCAPE="true"
+ 		fi
+
+		echo "Task Status" $TASKSTATUS
+		sleep 2
+	done
+	echo "Hbase Service Started..."
+
+elif [ "$HBASESTATUS" == STARTED ]; then
+	echo "Hbase Service  Started..."
+else
+	echo "Hbase Service in a transition state. Wait for process to complete and then run the install script again."
+	exit 1
+fi
+
 #Install and start Docker
 tee /etc/yum.repos.d/docker.repo <<-'EOF'
 [dockerrepo]
@@ -71,6 +104,38 @@ cp -vf metainfo.json /usr/hdp/docker/dockerbuild/transactionmonitorui
 cp -vf resources.json /usr/hdp/docker/dockerbuild/transactionmonitorui
 
 slider create transactionmonitorui --template /usr/hdp/docker/dockerbuild/transactionmonitorui/appConfig.json --metainfo /usr/hdp/docker/dockerbuild/transactionmonitorui/metainfo.json --resources /usr/hdp/docker/dockerbuild/transactionmonitorui/resources.json
+sleep 5
+
+STORMSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/STORM | grep '"state" :' | grep -Po '([A-Z]+)')
+
+if [ "$STORMSTATUS" == INSTALLED ]; then
+	echo "Starting Storm Broker..."
+	TASKID=$(curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Storm via REST"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/STORM | grep "id" | grep -Po '([0-9]+)')
+	echo "STORM TaskId " $TASKID
+	sleep 2
+
+	LOOPESCAPE="false"
+
+	until [ "$LOOPESCAPE" == true ]; do
+
+		TASKSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+		if [ "$TASKSTATUS" == COMPLETED ]; then
+			LOOPESCAPE="true"
+ 		fi
+		
+		echo "Task Status" $TASKSTATUS
+		sleep 2
+
+	done
+	echo "Storm Broker Started..."
+
+elif [ "$STORMSTATUS" == STARTED ]; then
+	echo "Storm Broker Started..."
+else
+	echo "Storm Broker in a transition state. Wait for process to complete and then run the install script again."
+	exit 1
+fi
+
 storm jar /home/storm/CreditCardTransactionMonitor-0.0.1-SNAPSHOT.jar com.hortonworks.iot.financial.topology.CreditCardTransactionMonitorTopology
 
 #Install NiFi Service in Ambari. Still need to log into Ambari and install the service from the console
