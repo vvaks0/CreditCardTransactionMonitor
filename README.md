7eff283acba6cf6d57152201dc90178513da96ec
vvaks <vvaks@hortonworks.com>
2016-04-22 00:29:39 +0100

    Added Automation

--- README.md
+++ README.md
@@ -4,22 +4,6 @@ Credit Card Transaction Monitor is an example of a Modern Data Application runni
 # Install CreditCardTransactionMonitor
 Download and Import Hortonworks Sandbox 2.4 for Virtual Box. Should work with VMWare but has not been tested. Modify local hosts file so that sandbox.hortonworks.com resolves to 127.0.0.1 (This is important and may break the simulator and UI) 
 
-Start Sandbox, SSH to Sandbox (ssh root@sandbox.hortonworks.com -p 2222)
-
-Change sandbox root password (passwd)
-
-Change Ambari password to "admin" (ambari-admin-password-reset) (Ambari passwords needs to be "admin" as scripts depend on it)
-
-cd /root (use the /root directory to begin the install)
-
-git clone https://github.com/vakshorton/CreditCardTransactionMonitor.git (make sure that git cloned to /root/CreditCardTransactionMonitor)
-
-cd /root/CreditCardTransactionMonitor
-
-./install.sh
-
-Reboot Sandbox (reboot now)
-
 Configure Virtual Box Port Forward
 
 8082 – HDF_HTTP_Ingest
@@ -30,17 +14,34 @@ Configure Virtual Box Port Forward
 
 9090 – HDF_Studio
 
+Start Sandbox, SSH to Sandbox (ssh root@sandbox.hortonworks.com -p 2222)
+
+Change sandbox root password (passwd)
+
+Change Ambari password to "admin" (ambari-admin-password-reset) 
+(!!!!!!!!!!!!!AMBARI PASSWORD MUST BE SET TO "admin" (no quotes)!!!!!!)
+(!!!!!!!!!!!!!INSTALL WILL NOT WORK WITH ANOTHER PASSWORD!!!!!!!!!!!!!)
+
+cd /root (use the /root directory to begin the install)
+
+git clone https://github.com/vakshorton/CreditCardTransactionMonitor.git (make sure that git cloned to /root/CreditCardTransactionMonitor)
+
+cd CreditCardTransactionMonitor
+
+./install.sh
+
+Install script will log out once complete. This is required to refresh session configuration.
+Log back in via SSH (ssh root@sandbox.hortonworks.com -p 2222)
+
 # Start Demo
-The script to start demo services should be located in the CreditCardTransactionMonitor directory
+The script to start demo services should be located in the /root/CreditCardTransactionMonitor directory
 ./startDemoServices.sh
 
 Slider will download the servlet (UI) docker containers from the docker hub so it may take a few minutes for the application server to start
 
-From the browser: http://sandbox.hortonworks.com:8090/TransactionMonitorUI/CustomerOverview
+Bring up the UI in a Browser: http://sandbox.hortonworks.com:8090/TransactionMonitorUI/CustomerOverview
 
-Copy the resulting jar to the host machine
-
-Start Simulation on Host (Not inside VM):
+Start Simulation:
 
 The Simulator should be located in the CreditCardTransactionMonitor directory
 
--- install.sh
+++ install.sh
@@ -1,13 +1,39 @@
 #!/bin/bash
-echo "Changing YARN Container Memory Size..."
+VERSION=`hdp-select status hadoop-client | sed 's/hadoop-client - \([0-9]\.[0-9]\).*/\1/'`
+INTVERSION=$(echo $VERSION*10 | bc | grep -Po '([0-9][0-9])')
+echo "*********************************SANDBOX VERSION IS $VERSION" 
+if [ "$INTVERSION" -lt 24 ]; then	
+	echo "*********************************Downloading NIFI..." 
+	sudo git clone https://github.com/abajwa-hw/ambari-nifi-service.git   /var/lib/ambari-server/resources/stacks/HDP/$VERSION/services/NIFI
+	service ambari restart
+else
+	echo "*********************************NO ACTION REQUIRED" 
+fi
+
+# Wait for Ambari
+LOOPESCAPE="false"
+until [ "$LOOPESCAPE" == true ]; do
+        TASKSTATUS=$(curl -u admin:admin -I -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox | grep -Po 'OK')
+        if [ "$TASKSTATUS" == OK ]; then
+                LOOPESCAPE="true"
+                TASKSTATUS="READY"
+        else
+                TASKSTATUS="PENDING"
+        fi
+		echo "Waiting for Ambari..."
+        echo "Ambari Status... " $TASKSTATUS
+        sleep 2
+done
+
+echo "*********************************Changing YARN Container Memory Size..."
 /var/lib/ambari-server/resources/scripts/configs.sh set sandbox.hortonworks.com Sandbox yarn-site "yarn.scheduler.maximum-allocation-mb" "6144"
 sleep 2
 /var/lib/ambari-server/resources/scripts/configs.sh set sandbox.hortonworks.com Sandbox yarn-site "yarn.nodemanager.resource.memory-mb" "6144"
 
 sleep 2
-echo "Restarting YARN..."
+echo "*********************************Restarting YARN..."
 TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context": "Stop YARN"}, "ServiceInfo": {"state": "INSTALLED"}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/YARN | grep "id" | grep -Po '([0-9]+)')
-echo "AMBARI TaskID " $TASKID
+echo "*********************************AMBARI TaskID " $TASKID
 sleep 2
 LOOPESCAPE="false"
 until [ "$LOOPESCAPE" == true ]; do
@@ -15,12 +41,12 @@ until [ "$LOOPESCAPE" == true ]; do
         if [ "$TASKSTATUS" == COMPLETED ]; then
                 LOOPESCAPE="true"
         fi
-        echo "Task Status" $TASKSTATUS
+        echo "*********************************Task Status" $TASKSTATUS
         sleep 2
 done
 
 TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context": "Start YARN"}, "ServiceInfo": {"state": "STARTED"}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/YARN | grep "id" | grep -Po '([0-9]+)')
-echo "AMBARI TaskID " $TASKID
+echo "*********************************AMBARI TaskID " $TASKID
 sleep 2
 LOOPESCAPE="false"
 until [ "$LOOPESCAPE" == true ]; do
@@ -28,21 +54,21 @@ until [ "$LOOPESCAPE" == true ]; do
         if [ "$TASKSTATUS" == COMPLETED ]; then
                 LOOPESCAPE="true"
         fi
-        echo "Task Status" $TASKSTATUS
+        echo "*********************************Task Status" $TASKSTATUS
         sleep 2
 done
 
-echo "Creating NIFI service..."
+echo "*********************************Creating NIFI service..."
 # Create NIFI service
 curl -u admin:admin -H "X-Requested-By:ambari" -i -X POST http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI
 
 sleep 2
-echo "Adding NIFI MASTER component..."
+echo "*********************************Adding NIFI MASTER component..."
 # Add NIFI Master component to service
 curl -u admin:admin -H "X-Requested-By:ambari" -i -X POST http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI/components/NIFI_MASTER
 
 sleep 2
-echo "Creating NIFI configuration..."
+echo "*********************************Creating NIFI configuration..."
 # Create and apply configuration
 VERSION=`hdp-select status hadoop-client | sed 's/hadoop-client - \([0-9]\.[0-9]\).*/\1/'`
 /var/lib/ambari-server/resources/scripts/configs.sh set sandbox.hortonworks.com Sandbox nifi-ambari-config /root/CreditCardTransactionMonitor/Nifi/config/nifi-ambari-config.json
@@ -56,15 +82,15 @@ sleep 2
 /var/lib/ambari-server/resources/scripts/configs.sh set sandbox.hortonworks.com Sandbox nifi-properties-env /root/CreditCardTransactionMonitor/Nifi/config/nifi-properties-env.json
 
 sleep 2
-echo "Adding NIFI MASTER role to Host..."
+echo "*********************************Adding NIFI MASTER role to Host..."
 # Add NIFI Master role to Sandbox host
 curl -u admin:admin -H "X-Requested-By:ambari" -i -X POST http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/hosts/sandbox.hortonworks.com/host_components/NIFI_MASTER
 
 sleep 2
-echo "Installing NIFI Service"
+echo "*********************************Installing NIFI Service"
 # Install NIFI Service
 TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Install Nifi"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "INSTALLED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI | grep "id" | grep -Po '([0-9]+)')
-echo "AMBARI TaskID " $TASKID
+echo "*********************************AMBARI TaskID " $TASKID
 sleep 2
 LOOPESCAPE="false"
 until [ "$LOOPESCAPE" == true ]; do
@@ -72,16 +98,16 @@ until [ "$LOOPESCAPE" == true ]; do
         if [ "$TASKSTATUS" == COMPLETED ]; then
                 LOOPESCAPE="true"
         fi
-        echo "Task Status" $TASKSTATUS
+        echo "*********************************Task Status" $TASKSTATUS
         sleep 2
 done
-echo "NIFI Service Installed..."
+echo "*********************************NIFI Service Installed..."
 
 sleep 2
-echo "Starting NIFI Service..."
+echo "*********************************Starting NIFI Service..."
 # Start NIFI service
 TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Start NIFI"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/NIFI | grep "id" | grep -Po '([0-9]+)')
-echo "AMBARI TaskID " $TASKID
+echo "*********************************AMBARI TaskID " $TASKID
 sleep 2
 LOOPESCAPE="false"
 until [ "$LOOPESCAPE" == true ]; do
@@ -89,59 +115,58 @@ until [ "$LOOPESCAPE" == true ]; do
         if [ "$TASKSTATUS" == COMPLETED ]; then
                 LOOPESCAPE="true"
         fi
-        echo "Task Status" $TASKSTATUS
+        echo "*********************************Task Status" $TASKSTATUS
         sleep 2
 done
-echo "NIFI Service Started..."
+echo "*********************************NIFI Service Started..."
 LOOPESCAPE="false"
 until [ "$LOOPESCAPE" == true ]; do
-
         TASKSTATUS=$(curl -u admin:admin -i -X GET http://sandbox.hortonworks.com:9090/nifi-api/controller | grep -Po 'OK')
         if [ "$TASKSTATUS" == OK ]; then
                 LOOPESCAPE="true"
         else
                 TASKSTATUS="PENDING"
         fi
-		echo "Waiting for NIFI Servlet..."
-        echo "NIFI Servlet Status... " $TASKSTATUS
+		echo "*********************************Waiting for NIFI Servlet..."
+        echo "*********************************NIFI Servlet Status... " $TASKSTATUS
         sleep 2
 done
 
-echo "Importing NIFI Template..."
+echo "*********************************Importing NIFI Template..."
 # Import NIFI Template
 #TEMPLATEID=$(curl -v -F template=@"/root/CreditCardTransactionMonitor/Nifi/template/CreditFraudDetectionFlow.xml" -X POST http://sandbox.hortonworks.com:9090/nifi-api/controller/templates | grep -Po '<id>([a-z0-9-]+)' | grep -Po '>([a-z0-9-]+)' | grep -Po '([a-z0-9-]+)')
 TEMPLATEID=$(curl -v -F template=@"Nifi/template/CreditFraudDetectionFlow.xml" -X POST http://sandbox.hortonworks.com:9090/nifi-api/controller/templates | grep -Po '<id>([a-z0-9-]+)' | grep -Po '>([a-z0-9-]+)' | grep -Po '([a-z0-9-]+)')
 sleep 2
-echo "Instantiating NIFI Flow..."
+echo "*********************************Instantiating NIFI Flow..."
 # Instantiate NIFI Template
 REVISION=$(curl -u admin:admin  -i -X GET http://sandbox.hortonworks.com:9090/nifi-api/controller/revision |grep -Po '\"version\":([0-9]+)' | grep -Po '([0-9]+)')
 curl -u admin:admin -i -H "Content-Type:application/x-www-form-urlencoded" -d "templateId=$TEMPLATEID&originX=100&originY=100&version=$REVISION" -X POST http://sandbox.hortonworks.com:9090/nifi-api/controller/process-groups/root/template-instance
 
-echo "Installing Maven"
+echo "*********************************Installing Maven"
 # Install Maven
 wget http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
 sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
 yum install -y apache-maven
 
-echo "Building Credit Card Transaction Monitor Storm Topology"
+echo "*********************************Building Credit Card Transaction Monitor Storm Topology"
 # Build from source
 cd CreditCardTransactionMonitor
 mvn clean package
 cp -vf target/CreditCardTransactionMonitor-0.0.1-SNAPSHOT.jar /home/storm
 
-echo "Building Credit Card Transaction Simulator"
+echo "*********************************Building Credit Card Transaction Simulator"
 # Build from source
 cd ../CreditCardTransactionSimulator
 mvn clean package
-cp -vf target/CreditCardTransactionMonitor-0.0.1-SNAPSHOT.jar ../
+cp -vf target/CreditCardTransactionSimulator-0.0.1-SNAPSHOT-jar-with-dependencies.jar ../
 
 #Start Kafka
 KAFKASTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/KAFKA | grep '"state" :' | grep -Po '([A-Z]+)')
 
 if [ "$KAFKASTATUS" == INSTALLED ]; then
-	echo "Starting Kafka Broker..."
+	echo "*********************************Starting Kafka Broker..."
 	TASKID=$(curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Kafka via REST"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/KAFKA | grep "id" | grep -Po '([0-9]+)')
-	echo "AMBARI TaskID " $TASKID
+	echo "*********************************AMBARI TaskID " $TASKID
 	sleep 2
 	LOOPESCAPE="false"
 	until [ "$LOOPESCAPE" == true ]; do
@@ -149,14 +174,14 @@ if [ "$KAFKASTATUS" == INSTALLED ]; then
 		if [ "$TASKSTATUS" == COMPLETED ]; then
 			LOOPESCAPE="true"
  		fi
-		echo "Task Status" $TASKSTATUS
+		echo "*********************************Task Status" $TASKSTATUS
 		sleep 2
 	done
-	echo "Kafka Broker Started..."
+	echo "*********************************Kafka Broker Started..."
 elif [ "$KAFKASTATUS" == STARTED ]; then
-	echo "Kafka Broker Started..."
+	echo "*********************************Kafka Broker Started..."
 else
-	echo "Kafka Broker in a transition state. Wait for process to complete and then run the install script again."
+	echo "*********************************Kafka Broker in a transition state. Wait for process to complete and then run the install script again."
 	exit 1
 fi
 
@@ -167,9 +192,9 @@ fi
 #Start HBASE
 HBASESTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/HBASE | grep '"state" :' | grep -Po '([A-Z]+)')
 if [ "$HBASESTATUS" == INSTALLED ]; then
-	echo "Starting  Hbase Service..."
+	echo "*********************************Starting  Hbase Service..."
 	TASKID=$(curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Hbase via REST"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/HBASE | grep "id" | grep -Po '([0-9]+)')
-	echo "HBASE TaskId " $TASKID
+	echo "*********************************HBASE TaskId " $TASKID
 	sleep 2
 	LOOPESCAPE="false"
 	until [ "$LOOPESCAPE" == true ]; do
@@ -177,14 +202,14 @@ if [ "$HBASESTATUS" == INSTALLED ]; then
 		if [ "$TASKSTATUS" == COMPLETED ]; then
 			LOOPESCAPE="true"
  		fi
-		echo "Task Status" $TASKSTATUS
+		echo "*********************************Task Status" $TASKSTATUS
 		sleep 2
 	done
-	echo "Hbase Service Started..."
+	echo "*********************************Hbase Service Started..."
 elif [ "$HBASESTATUS" == STARTED ]; then
-	echo "Hbase Service  Started..."
+	echo "*********************************Hbase Service  Started..."
 else
-	echo "Hbase Service in a transition state. Wait for process to complete and then run the install script again."
+	echo "*********************************Hbase Service in a transition state. Wait for process to complete and then run the install script again."
 	exit 1
 fi
 
@@ -219,9 +244,9 @@ cp -vf resources.json /home/docker/dockerbuild/transactionmonitorui
 
 STORMSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/STORM | grep '"state" :' | grep -Po '([A-Z]+)')
 if [ "$STORMSTATUS" == INSTALLED ]; then
-	echo "Starting Storm Service..."
+	echo "*********************************Starting Storm Service..."
 	TASKID=$(curl -u admin:admin -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Storm via REST"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/Sandbox/services/STORM | grep "id" | grep -Po '([0-9]+)')
-	echo "STORM TaskId " $TASKID
+	echo "*********************************STORM TaskId " $TASKID
 	sleep 2
 	LOOPESCAPE="false"
 	until [ "$LOOPESCAPE" == true ]; do
@@ -229,19 +254,19 @@ if [ "$STORMSTATUS" == INSTALLED ]; then
 		if [ "$TASKSTATUS" == COMPLETED ]; then
 			LOOPESCAPE="true"
  		fi
-		echo "Task Status" $TASKSTATUS
+		echo "*********************************Task Status" $TASKSTATUS
 		sleep 2
 	done
-	echo "Storm Broker Started..."
+	echo "*********************************Storm Broker Started..."
 elif [ "$STORMSTATUS" == STARTED ]; then
-	echo "Storm Service Started..."
+	echo "*********************************Storm Service Started..."
 else
-	echo "Storm Service in a transition state. Wait for process to complete and then run the install script again."
+	echo "*********************************Storm Service in a transition state. Wait for process to complete and then run the install script again."
 	exit 1
 fi
 
 # Start NIFI Flow
-echo "Starting NIFI Flow..."
+echo "*********************************Starting NIFI Flow..."
 REVISION=$(curl -u admin:admin  -i -X GET http://sandbox.hortonworks.com:9090/nifi-api/controller/revision |grep -Po '\"version\":([0-9]+)' | grep -Po '([0-9]+)')
 TARGETS=($(curl -u admin:admin -i -X GET http://sandbox.hortonworks.com:9090/nifi-api/controller/process-groups/root/processors | grep -Po '\"uri\":\"([a-z0-9-://.]+)' | grep -Po '(?!.*\")([a-z0-9-://.]+)'))
 length=${#TARGETS[@]}
@@ -253,7 +278,17 @@ for ((i = 0; i != length; i++)); do
 done
 
 # Deploy Storm Topology
+echo "*********************************Deploying Storm Topology..."
 storm jar /home/storm/CreditCardTransactionMonitor-0.0.1-SNAPSHOT.jar com.hortonworks.iot.financial.topology.CreditCardTransactionMonitorTopology
+
+echo "*********************************Downloading Docker Images for UI..."
+# Download Docker Images
+service docker start
+docker pull vvaks/transactionmonitorui
+docker pull vvaks/cometd
+
+# Logout to refresh session configuration
+logout
 
 #slider create transactionmonitorui --template /home/docker/dockerbuild/transactionmonitorui/appConfig.json --metainfo /home/docker/dockerbuild/transactionmonitorui/metainfo.json --resources /home/docker/dockerbuild/transactionmonitorui/resources.json
 #sleep 5
