28a7cf2a8daa8ce55f08298768a973bf681ccc1f
vvaks <vvaks@hortonworks.com>
2016-07-07 23:56:43 -0400

    update automation

--- startDemoServices.sh
+++ startDemoServices.sh
@@ -121,6 +121,66 @@ else
 	exit 1
 fi
 
+curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Start Log Search"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/services/LOGSEARCH
+
+# Start Log Search
+LOGSEARCHSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/services/LOGSEARCH | grep '"state" :' | grep -Po '([A-Z]+)')
+if [ "$LOGSEARCHSTATUS" == INSTALLED ]; then
+	echo "Starting Log Search Service..."
+	TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Start Log Search"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/services/LOGSEARCH | grep "id" | grep -Po '([0-9]+)')
+	echo "LOGSEARCHSTATUS TaskId " $TASKID
+	sleep 2
+
+	LOOPESCAPE="false"
+
+	until [ "$LOOPESCAPE" == true ]; do
+
+		TASKSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+		if [ "$TASKSTATUS" == COMPLETED ]; then
+			LOOPESCAPE="true"
+ 		fi
+		
+		echo "Task Status" $TASKSTATUS
+		sleep 2
+	done
+	echo "Log Search Service Started..."
+
+elif [ "$LOGSEARCHSTATUS" == STARTED ]; then
+	echo "Log Search Service Started..."
+else
+	echo "Log Search Service in a transition state. Wait for process to complete and then run the install script again."
+	exit 1
+fi
+
+# Start Atlas
+ATLASSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/services/ATLAS | grep '"state" :' | grep -Po '([A-Z]+)')
+if [ "$ATLASSTATUS" == INSTALLED ]; then
+	echo "Starting Atlas Service..."
+	TASKID=$(curl -u admin:admin -H "X-Requested-By:ambari" -i -X PUT -d '{"RequestInfo": {"context" :"Start Log Search"}, "Body": {"ServiceInfo": {"maintenance_state" : "OFF", "state": "STARTED"}}}' http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/services/ATLAS | grep "id" | grep -Po '([0-9]+)')
+	echo "ATLASSTATUS TaskId " $TASKID
+	sleep 2
+
+	LOOPESCAPE="false"
+
+	until [ "$LOOPESCAPE" == true ]; do
+
+		TASKSTATUS=$(curl -u admin:admin -X GET http://sandbox.hortonworks.com:8080/api/v1/clusters/$CLUSTER_NAME/requests/$TASKID | grep "request_status" | grep -Po '([A-Z]+)')
+		if [ "$TASKSTATUS" == COMPLETED ]; then
+			LOOPESCAPE="true"
+ 		fi
+		
+		echo "Task Status" $TASKSTATUS
+		sleep 2
+	done
+	echo "Atlas Service Started..."
+
+elif [ "$ATLASSTATUS" == STARTED ]; then
+	echo "Atlas Service Started..."
+else
+	echo "Atlas Service in a transition state. Wait for process to complete and then run the install script again."
+	exit 1
+fi
+
 # Clear Slider working directory
 sudo -u hdfs hadoop fs -rm -R /user/root/.slider/cluster
 # Ensure docker service is running
