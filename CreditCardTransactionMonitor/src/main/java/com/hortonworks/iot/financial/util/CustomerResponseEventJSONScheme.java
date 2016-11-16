aad3fe0cbc4dc21b8f87323cc4698db5e78e8a84
vvaks <vvaks@hortonworks.com>
2016-11-15 22:45:37 -0500

    update

--- CreditCardTransactionMonitor/src/main/java/com/hortonworks/iot/financial/util/CustomerResponseEventJSONScheme.java
+++ CreditCardTransactionMonitor/src/main/java/com/hortonworks/iot/financial/util/CustomerResponseEventJSONScheme.java
@@ -15,6 +15,7 @@ import com.hortonworks.iot.financial.events.IncomingTransaction;
 import backtype.storm.spout.Scheme;
 import backtype.storm.tuple.Fields;
 import backtype.storm.tuple.Values;
+import storm.kafka.KeyValueScheme;
 
 /*
 import org.apache.storm.spout.Scheme;
@@ -22,7 +23,7 @@ import org.apache.storm.tuple.Fields;
 import org.apache.storm.tuple.Values;
 */
 
-public class CustomerResponseEventJSONScheme implements Scheme {
+public class CustomerResponseEventJSONScheme implements KeyValueScheme  {
 		private static final long serialVersionUID = 1L;
 		private static final Charset UTF8 = Charset.forName("UTF-8");
 
@@ -46,4 +47,29 @@ public class CustomerResponseEventJSONScheme implements Scheme {
 	    public Fields getOutputFields() {
 	        return new Fields("CustomerResponse");
 	    }
+
+		@Override
+		public List<Object> deserializeKeyAndValue(byte[] key, byte[] value) {
+			String keyString = new String(key, UTF8); 
+			String eventJSONString = new String(value, UTF8);
+		        CustomerResponse customerResponse = null;
+		        ObjectMapper mapper = new ObjectMapper();
+		        
+		        try {
+					customerResponse = mapper.readValue(eventJSONString, CustomerResponse.class);
+				} catch (JsonParseException e) {
+					e.printStackTrace();
+				} catch (JsonMappingException e) {
+					e.printStackTrace();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+		        return new Values(customerResponse);
+
+		}
+		
+		public List<Object> deserializeKeyAndValue(ByteBuffer arg0, ByteBuffer arg1) {
+			// TODO Auto-generated method stub
+			return null;
+		}
 }
