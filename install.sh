f7be18cad81c475a5b4fe2e348b50dd9e2aa9e1e
vvaks <vvaks@hortonworks.com>
2016-11-28 00:47:45 -0500

    update

--- install.sh
+++ install.sh
@@ -488,7 +488,7 @@ getAtlasHost () {
 }
 
 getNifiHost () {
-       	NIFI_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/comonents/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
+       	NIFI_HOST=$(curl -u admin:admin -X GET http://$AMBARI_HOST:8080/api/v1/clusters/$CLUSTER_NAME/services/NIFI/components/NIFI_MASTER|grep "host_name"|grep -Po ': "([a-zA-Z0-9\-_!?.]+)'|grep -Po '([a-zA-Z0-9\-_!?.]+)')
        	
        	echo $NIFI_HOST
 }
